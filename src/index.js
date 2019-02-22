4ae2dc892f902ac5f66220dfb3b3ca7717dadc33
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-02-21 23:04:13 -0300

    fontello antigo removido

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './fontello.css';
-import './fonts/fontello.css'
 import App from './App';
 import * as serviceWorker from './scripts/serviceWorker';
 
