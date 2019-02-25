3adcf0240caafe451e6265130a82f675c3479afd
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-02-25 18:11:54 -0300

    pagina de notificacoes feita

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -5,8 +5,43 @@ import Header from '../components/app/Header'
 function Notifications () {
   return (
     <div>
-      <Header navBarContent={['back', 'space', 'citySelection', 'notifications', 'menu']}/>
+      <Header title="Notificações" navBarContent={['back', 'title', 'space']}/>
 
+
+      <ul className="notificationList">
+        <li>
+          <a href="">
+            <img src="../assets/icons/check.png" alt="" className="icon"/>
+            <span className="content">
+              Sua consulta com <b>Dr. Rodrigo Arruda</b> foi confirmada, confira os detalhes.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="../assets/icons/warning.png" alt="" className="icon"/>
+            <span className="content">
+              Sua consulta com <b>Dr. Rodrigo Arruda</b> teve uma alteração, confira os detalhes.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="../assets/icons/question.png" alt="" className="icon"/>
+            <span className="content">
+              <b>Felipe</b>, como foi sua consulta com <b>Dr. Rodrigo Arruda</b>? ajude-nos a melhorar sua experiência.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="../assets/icons/discounts.png" alt="" className="icon"/>
+            <span className="content">
+              Olá <b>Felipe</b>, a <b>Droga Rocha</b> enviou uma oferta para você, confira!
+            </span>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
