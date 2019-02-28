0f51e8504faac234660d27649d6285d67965eb41
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-02-27 21:40:01 -0300

    searchbar consertada

--- src/components/app/SearchBar.js
+++ src/components/app/SearchBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'
+import {useFormState} from 'react-use-form-state'
 
-function SearchBar() {
+function SearchBar({history}) {
+  const [formState, { text }] = useFormState()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    history.push(`/pesquisa/${formState.values.query}`)
+  }
+
   return (
-    <form className="searchForm" action="pesquisa/algumacoisa">
-      <input type="text" className="input" name="query" placeholder="Busca" />
+    <form className="searchForm" onSubmit={handleSubmit}>
+      <input {...text('query')} type="text" className="input" placeholder="Busca" />
       <button className="submit">
         <i className="icon-search" />
       </button>
@@ -11,4 +20,4 @@ function SearchBar() {
   );
 }
 
-export default SearchBar;
+export default withRouter(SearchBar);
