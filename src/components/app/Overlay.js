549835982a8d98cd901944e27b5a365c429eda96
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-03-18 19:26:46 -0300

    overlay corrigido

--- src/components/app/Overlay.js
+++ src/components/app/Overlay.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-function Overlay ({ close,active,children, className }) {
-  const handleClick = event => (event.target === event.currentTarget) && close()
+function Overlay({ close, active, children, className }) {
+  const handleClick = event => event.target === event.currentTarget && close();
 
   return (
     <StyledOverlay onClick={handleClick} active={active} className={className}>
       {children}
     </StyledOverlay>
-  )
+  );
 }
 
 const StyledOverlay = styled.div`
@@ -17,11 +17,11 @@ const StyledOverlay = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0,0,0,0.8);
+  background-color: rgba(0, 0, 0, 0.8);
   z-index: 15;
-  transition: all .5s ease;
-  opacity: ${props => props.active ? 1 : 0};
-  visibility: ${props => props.active ? 'visible' : 'hidden'};
-`
+  transition: all 0.5s ease;
+  opacity: ${props => (props.active ? 1 : 0)};
+  visibility: ${props => (props.active ? 'visible' : 'hidden')};
+`;
 
-export default Overlay
+export default Overlay;
