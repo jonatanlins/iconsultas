8c001642f73f00f283dd05941853c54951a70610
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-03-21 22:59:03 -0300

    zoom removido no clique duplo

--- src/scripts/disableZoom.js
+++ src/scripts/disableZoom.js
@@ -1,5 +1,6 @@
 function disableZoom() {
   document.addEventListener('gesturestart', e => e.preventDefault());
+  document.addEventListener('dblclick', e => e.preventDefault());
 }
 
 export default disableZoom;
